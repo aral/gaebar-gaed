b35b7f24295659d491cc2137f2cf959d4f0667e7
Aral Balkan <aral@aralbalkan.com>
2008-12-22 15:38:47 +0000

    Removed comments from urls.

--- urls.py
+++ urls.py
@@ -17,12 +17,6 @@ from django.conf.urls.defaults import *
 from app1 import views as app1_views
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^foo/', include('foo.urls')),
-
-    # Uncomment this for admin:
-    # (r'^admin/', include('django.contrib.admin.urls')),
-
     url(r'^gaebar/', include('gaebar.urls')),
     url(r'^populate-datastore', view=app1_views.populate_datastore),
     url(r'^run-tests', view=app1_views.run_tests),
